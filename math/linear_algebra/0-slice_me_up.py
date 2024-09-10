fe341d8b92dd81e0f6c4083b699e9ba830feaa5f
Peter Philip Johnson <125555434+maxprodigy@users.noreply.github.com>
2024-09-11 01:10:03 +0200

    Update 0-slice_me_up.py

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
-arr1 = arr[:2] # Get first two elements (0-based indexing)
-arr2 = arr[-5:] # Get last five elements
-arr3 = arr[1:6] # Get elements from index 1 (inclusive) to 5 (exclusive)
+arr1 = arr[:2]  # First two elements
+arr2 = arr[-5:]  # Last five elements
+arr3 = arr[1:6]  # Elements from index 1 (inclusive) to 6 (exclusive)
 print("The first two numbers of the array are:", arr1)
 print("The last five numbers of the array are:", arr2)
 print("The 2nd through 6th numbers of the array are:", arr3)
